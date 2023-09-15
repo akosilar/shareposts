f767a5c9065c92d640213554fd12cbc0ce1855ab
akosilar <55524614+akosilar@users.noreply.github.com>
2023-09-14 22:47:44 -0400

    created user model, and email validation check against db

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -1,7 +1,7 @@
 <?php
     class Users extends Controller {
         public function __construct(){
-
+            $this->userModel = $this->model('User');
         }
 
         public function register(){
@@ -27,6 +27,12 @@
                 // validate email
                 if(empty($data['email'])){
                     $data['email_err'] = 'Please enter email';
+                } else {
+                    // check email
+                    if($this->userModel->findUserByEmail($data['email'])){
+                        $data['email_err'] = 'Email is already taken';
+
+                    }
                 }
 
                 // validate name
