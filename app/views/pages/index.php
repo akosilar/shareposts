4f29a0703d7bc5571e8daeb27cafe2dc19988852
akosilar <55524614+akosilar@users.noreply.github.com>
2023-09-14 20:01:46 -0400

    Initial commit

---
+++ app/views/pages/index.php
@@ -0,0 +1,9 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+
+<h1><?php echo $data['title'];?></h1>
+
+<p>This is the LarsMVC PHP framework. Please refer to docs.</p>
+
+
+
+<?php require APPROOT . '/views/inc/footer.php'; ?>
